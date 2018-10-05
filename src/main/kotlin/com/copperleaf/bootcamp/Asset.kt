5ac60f5893ea4a71bda1e5ae0b96f3ef53fc440f
Shirley Yin <SYin@copperleaf.com>
2018-10-05 11:52:08 -0700

    Asset class and rest controller that returns list of Assets

---
+++ src/main/kotlin/com/copperleaf/bootcamp/Asset.kt
@@ -0,0 +1,3 @@
+package com.copperleaf.bootcamp
+
+class Asset(val id: Int, val name: String)
---
+++ src/main/kotlin/com/copperleaf/bootcamp/AssetController.kt
@@ -0,0 +1,16 @@
+package com.copperleaf.bootcamp
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AssetController {
+
+    @GetMapping("/assets")
+    fun getAssets(): List<Asset> {
+        val assets: List<Asset> = (0..4).map { it -> Asset(id = it, name = "Asset #" + (it + 1)) }
+
+        return assets
+    }
+
+}
