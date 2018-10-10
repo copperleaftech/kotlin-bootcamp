c9eb3ce39a81a5d5ba300ab1c76af365951fecab
Shirley Yin <syin@copperleaf.com>
2018-10-09 17:43:25 -0700

    failed attempt to fix liquibase in build

--- src/main/kotlin/com/copperleaf/bootcamp/Asset.kt
+++ src/main/kotlin/com/copperleaf/bootcamp/Asset.kt
@@ -1,3 +1,19 @@
 package com.copperleaf.bootcamp
 
-class Asset(val id: Int, val name: String)
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotNull
+
+@Entity
+class Asset(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+
+    @NotNull
+    val name: String
+
+)
