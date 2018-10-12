982bdf5c3abe678e0897b0528a87fb24c1253d43
Shirley Yin <syin@copperleaf.com>
2018-10-12 10:07:27 -0700

    use spring data rest

--- src/main/kotlin/com/copperleaf/bootcamp/AssetRepository.kt
+++ src/main/kotlin/com/copperleaf/bootcamp/AssetRepository.kt
@@ -1,7 +1,7 @@
 package com.copperleaf.bootcamp
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "assets", path = "assets")
 interface AssetRepository : JpaRepository<Asset, Long>
