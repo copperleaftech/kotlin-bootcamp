83dc993997d8c5779c7ab8715142a5d342d890c1
Shirley Yin <syin@copperleaf.com>
2018-10-16 12:22:10 -0700

    more templates wip

--- templates/src/app/app.module.ts
+++ templates/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InvestmentFormComponent } from './investment-form/investment-form.component';
-import { AssetsComponent } from './assets/assets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvestmentFormComponent,
-    AssetsComponent
+    InvestmentFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- templates/src/app/investment-form/investment-form.component.ts
+++ templates/src/app/investment-form/investment-form.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-investment-form',
   templateUrl: './investment-form.component.html',
   styleUrls: ['./investment-form.component.css']
 })
+@Injectable()
 export class InvestmentFormComponent implements OnInit {
 
-  apiUrl = 'http://localhost:8080/v1/investments'
+  apiUrlBase = 'http://localhost:8080/v1/'
+  investmentsApiUrl = this.apiUrlBase + 'investments'
+  assetsApiUrl = this.apiUrlBase + 'assets'
 
-  constructor() { }
+  assets = []; // is this necessary?
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log('init');
+    this.getAssets();
+  }
+
+  getAssets() {
+    console.log('getAssets');
+    this.http.get(this.assetsApiUrl).subscribe((data) => {
+      console.log('data', data._embedded.assets);
+      this.assets = data._embedded.assets;
+    });
   }
 
   onSubmit() {
-    console.log('Submitted')
+    console.log('Submitted');
+    // console.log(formValue);
+
+
+  //  this.http.post(this.investmentsApiUrl)
   }
 
 }
