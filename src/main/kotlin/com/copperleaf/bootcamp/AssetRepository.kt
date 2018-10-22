3e5742e956f8924a6f5dbbd881dcce2b04352a99
Shirley Yin <syin@copperleaf.com>
2018-10-22 14:19:33 -0700

    use CrudRepository instead of JpaRepository, add newlines to end of files

--- src/main/kotlin/com/copperleaf/bootcamp/AssetRepository.kt
+++ src/main/kotlin/com/copperleaf/bootcamp/AssetRepository.kt
@@ -1,13 +1,13 @@
 package com.copperleaf.bootcamp
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.web.bind.annotation.CrossOrigin
 
 @CrossOrigin(origins = ["http://localhost", "http://192.168.11.55"])
 @RepositoryRestResource(collectionResourceRel = "assets", path = "assets")
-interface AssetRepository : JpaRepository<Asset, Int> {
+interface AssetRepository : CrudRepository<Asset, Int> {
 
     @RestResource(path = "all")
     fun queryByNameContainingOrTypeContainingOrLocationContainingAllIgnoreCase(name: String, type: String, location: String): List<Asset>?
--- src/main/kotlin/com/copperleaf/bootcamp/InvestmentRepository.kt
+++ src/main/kotlin/com/copperleaf/bootcamp/InvestmentRepository.kt
@@ -1,9 +1,9 @@
 package com.copperleaf.bootcamp
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.web.bind.annotation.CrossOrigin
 
 @CrossOrigin(origins = ["http://localhost", "http://192.168.11.55"])
 @RepositoryRestResource(collectionResourceRel = "investments", path = "investments")
-interface InvestmentRepository : JpaRepository<Investment, Int>
+interface InvestmentRepository : CrudRepository<Investment, Int>
